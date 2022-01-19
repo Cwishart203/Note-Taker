e1f475498c2a8ba8090a309fc960f69c6dd68e90
Cwishart203 <93050589+Cwishart203@users.noreply.github.com>
2022-01-19 09:17:21 -0500

    Set up server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
+const app = express();
+const PORT = process.env.PORT || 3001
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/", htmlRoutes);
+app.use("/api", apiRoutes);
+
+app.listen(PORT, () =>
+    console.log("Sucess")
+);
+
+
